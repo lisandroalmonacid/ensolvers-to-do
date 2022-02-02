8319eddcba3742ac8c060d9e75ef9b004314a6ed
lisandroalmonacid <66971017+lisandroalmonacid@users.noreply.github.com>
2022-02-02 19:08:37 -0300

    Delete backend/ensolvers-to-do-api/config directory

--- backend/ensolvers-to-do-api/config/apis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import my_view
-
-urlpatterns = [
-    path('', my_view),
-]
--- backend/ensolvers-to-do-api/config/todoapi/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Todo(models.Model):
-    sessionid = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-    done = models.BooleanField()
-
-    def __str__(self):
-        return self.title
