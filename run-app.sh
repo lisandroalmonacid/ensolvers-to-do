73dfca9b9324aff8ee5e2f5daddc35d95639b7f3
lisandroalmonacid <66971017+lisandroalmonacid@users.noreply.github.com>
2022-02-02 21:08:15 -0300

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,1 +1,12 @@
+#install dependencies
 
+sudo apt install nodejs
+sudo apt install npm
+npm install -g @vue/cli
+npm install -g @nestjs/cli
+
+#run backend
+gnome-terminal -- cd todo-backend && npm run start
+
+#run frontend
+gnome-terminal -- cd frontend/ensolvers-to-do-frontend && npm run serve
