5e138e3b26b4184f687f29e5d6c065f4da563dbe
lisandroalmonacid <66971017+lisandroalmonacid@users.noreply.github.com>
2022-02-02 22:47:57 -0300

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -5,6 +5,8 @@ sudo apt install npm
 npm install -g @vue/cli
 npm install -g @nestjs/cli
 
+#install backend dependencies
+gnome-terminal --wait -e '/bin/bash -c "cd todo-backend && npm install" '
 
 #run backend
 gnome-terminal -e '/bin/bash -c "cd todo-backend && npm run start" '
